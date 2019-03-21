e6518d1fde00705fea2591f425e691c13360f98f
Clifford Lee <leecliff@usc.edu>
2019-03-20 17:02:09 -0700

    [part1] passing all tests

--- src/com/chunkserver/ChunkServer.java
+++ src/com/chunkserver/ChunkServer.java
@@ -2,6 +2,14 @@ package com.chunkserver;
 
 import com.interfaces.ChunkServerInterface;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * implementation of interfaces at the chunkserver side
  * 
@@ -11,15 +19,18 @@ import com.interfaces.ChunkServerInterface;
 
 public class ChunkServer implements ChunkServerInterface {
 	final static String filePath = "C:\\Users\\shahram\\Documents\\TinyFS-2\\csci485Disk\\"; // or C:\\newfile.txt
+	final static String osxFilePath = "/Volumes/SD/edu/cs485/project/temp/";	// temp file to be used on local development
 	public static long counter;
 
 	/**
 	 * Initialize the chunk server
 	 */
 	public ChunkServer() {
-		System.out.println(
-				"Constructor of ChunkServer is invoked:  Part 1 of TinyFS must implement the body of this method.");
-		System.out.println("It does nothing for now.\n");
+		System.out.println("Initializing chunk server...");
+		
+		// initialize static counter to number of files in ChunkServer dir
+		// ChunkServer.counter = new File(filePath).list().length;
+		ChunkServer.counter = new File(osxFilePath).list().length;
 	}
 
 	/**
@@ -27,9 +38,31 @@ public class ChunkServer implements ChunkServerInterface {
 	 * in the file.
 	 */
 	public String initializeChunk() {
-		System.out.println("createChunk invoked:  Part 1 of TinyFS must implement the body of this method.");
-		System.out.println("Returns null for now.\n");
-		return null;
+		// initialize as a null string
+		String chunkHandle = null;
+		RandomAccessFile chunkFile = null;
+		
+		try {
+			// generate a valid chunk handle
+			chunkHandle = String.format("%s_%d", InetAddress.getLocalHost().getHostAddress(), counter);
+			++counter;
+
+			// create a chunk file
+//			chunkFile = new RandomAccessFile(filePath + chunkHandle, "rw");
+			chunkFile = new RandomAccessFile(osxFilePath + chunkHandle, "rw");
+			chunkFile.setLength(ChunkServer.ChunkSize);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			try {
+				chunkFile.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return chunkHandle;
 	}
 
 	/**
@@ -37,17 +70,58 @@ public class ChunkServer implements ChunkServerInterface {
 	 * should be no greater than 4KB
 	 */
 	public boolean putChunk(String ChunkHandle, byte[] payload, int offset) {
-		System.out.println("writeChunk invoked:  Part 1 of TinyFS must implement the body of this method.");
-		System.out.println("Returns false for now.\n");
-		return false;
+		RandomAccessFile chunk = null;
+		
+		try {
+			// check if chunk handle is valid & file exists
+//			File chunkFile = new File(filePath + ChunkHandle);
+			File chunkFile = new File(osxFilePath + ChunkHandle);
+			if (!chunkFile.exists()) {
+				return false;
+			}
+
+			// load file with chunk handle
+			chunk = new RandomAccessFile(chunkFile, "rw");
+
+			// write payload at offset for given length of payload
+			chunk.write(payload, offset, payload.length);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (chunk != null) {
+				try {
+					chunk.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return true;
 	}
 
 	/**
 	 * read the chunk at the specific offset
 	 */
 	public byte[] getChunk(String ChunkHandle, int offset, int NumberOfBytes) {
-		System.out.println("readChunk invoked:  Part 1 of TinyFS must implement the body of this method.");
-		System.out.println("Returns null for now.\n");
+		RandomAccessFile chunk = null;
+		try {
+			chunk = new RandomAccessFile(osxFilePath + ChunkHandle, "r");
+			
+			byte[] buffer = new byte[NumberOfBytes];
+			chunk.read(buffer, offset, NumberOfBytes);
+			
+			return buffer;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				chunk.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return null;
 	}
 
